3e11e27160e53608dfbff38b5b007cf0e9f1aa95
torresemili19-lab <torresemili19@gmail.com>
2026-01-22 15:02:58 -0300

    atualizacao

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,34 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Indicamos a configuração do Swagger
   const config = new DocumentBuilder()
-  .setTitle('Blog Pessoal')
-  .setDescription('Projeto Blog Pessoal')
-  .setContact("Generation Brasil","http://www.generationbrasil.online","generation@email.com")
-  .setVersion('1.0')
-  .addBearerAuth()
-  .build();
+    .setTitle('Blog Pessoal')
+    .setDescription('Projeto Blog Pessoal')
+    .setContact("Generation Brasil", "http://www.generationbrasil.online", "generation@email.com")
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger', app, document);
-  
+
+  // Ajustando o Fuso Horário do BD
   process.env.TZ = '-03:00';
 
+  // Aplicando os recursos de validação
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors()
+
+  // Habilitando o CORS do projeto
+  app.enableCors();
+
+  // Indico qual porta o projeto está sendo executado
   await app.listen(4000);
 }
+
 bootstrap();
