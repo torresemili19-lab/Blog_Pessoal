2e1341205d1d3a1df3b2182a9990e7986e20550f
torresemili19-lab <torresemili19@gmail.com>
2026-01-22 14:15:18 -0300

    atualizacao

--- src/tema/entities/tema.entity.ts
+++ src/tema/entities/tema.entity.ts
@@ -1,22 +1,23 @@
-import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Postagem } from "../../postagem/entities/postagem.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
-@Entity({name: "tb_temas"})
+@Entity({ name: "tb_temas" })
 export class Tema {
 
-    @PrimaryGeneratedColumn()  
-    @ApiProperty()   
+    @PrimaryGeneratedColumn()
+    @ApiProperty()
     id: number
 
     @IsNotEmpty()
-    @Column({length: 255, nullable: false})
-    @ApiProperty() 
+    @Column({ length: 255, nullable: false })
+    @ApiProperty()
     descricao: string
 
-    @ApiProperty() 
+    // Indica o lado UM do relacionamento, indicando que esse campo se conecta ao campo Usuario da Model Postagem
     @OneToMany(() => Postagem, (postagem) => postagem.tema)
+    @ApiProperty()
     postagem: Postagem[]
-    
+
 }
--- src/tema/tema.modules.ts
+++ src/tema/tema.modules.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Tema } from './entities/tema.entity';
-import { TemaService } from './services/tema.service';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { TemaController } from "./controllers/tema.controller";
+import { Tema } from "./entities/tema.entity";
+import { TemaService } from "./services/tema.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tema])],
-  providers: [TemaService],
-  exports: [TemaService], // exporta para ser usado em PostagemModule
+    imports: [TypeOrmModule.forFeature([Tema])],
+    providers: [TemaService],
+    controllers: [TemaController],
+    exports: [TypeOrmModule]
 })
 export class TemaModule {}
